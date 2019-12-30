f7fa7895cf3e534cac22a00b318efe964166a413
Joseph Miller <joff@joff.codes>
2019-12-30 14:09:15 +1100

    More WIP

--- internal-certificates/generate-certificate.sh
+++ internal-certificates/generate-certificate.sh
@@ -2,6 +2,18 @@
 
 set -eo pipefail
 
-: "${1:?"Need to pass in the Buildkite pipeline slug"}"
-SLUG=$1
-CERTIFICATE_BUCKET="oculo-services-buildkite-secrets/${SLUG}/certs"
+echo "First, we need some details from you:"
+echo
+
+read -p "Buildkite project slug for your app: " slug
+CERTIFICATE_BUCKET="oculo-services-buildkite-secrets/${slug}/certs"
+
+read -p "Application Name: (e.g. doc-renderer) " app_name
+
+read -p "Application Environment (e.g. uk-production) " app_env
+
+read -p "ECS cluster you are deploying to (e.g.) uk-prod01: " ecs_cluster
+
+DOMAIN="${app_name}-${app_env}.${ecs_cluster}-private-service-discovery"
+
+minica -ca-cert /root-certificate/minica.pem -ca-key /root-certificate/minica-key.pem -domains "${DOMAIN}"
