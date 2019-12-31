ed36563e80da851da25b0985737dcc5fcfe10eaf
Joseph Miller <joff@joff.codes>
2019-12-31 15:42:10 +1100

    Give over more to the RAMDisk, and don't let buildkite flip out so much when the space gets low

--- packer/oculo-scripts/add-ramdisk-tmpfs.sh
+++ packer/oculo-scripts/add-ramdisk-tmpfs.sh
@@ -2,5 +2,6 @@
 
 set -euxo pipefail
 
-echo 'none /var/lib/docker tmpfs size=8G' | sudo tee -a /etc/fstab >/dev/null
-echo 'none /var/lib/buildkite-agent/builds tmpfs size=8G' | sudo tee -a /etc/fstab >/dev/null
+echo 'none /var/lib/docker tmpfs size=12G' | sudo tee -a /etc/fstab >/dev/null
+echo 'none /var/lib/buildkite-agent/builds tmpfs size=12G' | sudo tee -a /etc/fstab >/dev/null
+echo 'export DISK_MIN_AVAILABLE=976563' | sudo tee -a /var/lib/buildkite-agent/.bashrc >/dev/null
