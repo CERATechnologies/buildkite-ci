6adae714c9272acc1161a04ee8f744de26bab5ba
Joseph Miller <joff@joff.codes>
2019-12-31 14:40:39 +1100

    Use more RAM, muahahahahaha

--- packer/oculo-scripts/add-ramdisk-tmpfs.sh
+++ packer/oculo-scripts/add-ramdisk-tmpfs.sh
@@ -2,4 +2,5 @@
 
 set -euxo pipefail
 
-echo 'none /var/lib/docker tmpfs size=10G' | sudo tee -a /etc/fstab > /dev/null
+echo 'none /var/lib/docker tmpfs size=8G' | sudo tee -a /etc/fstab >/dev/null
+echo 'none /var/lib/buildkite-agent/builds tmpfs size=8G' | sudo tee -a /etc/fstab >/dev/null
