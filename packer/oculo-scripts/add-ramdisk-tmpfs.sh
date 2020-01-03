2d8b6f8eb583bf88115fd8bdd7cb1264c9ce2ab0
Joseph Miller <joff@joff.codes>
2020-01-03 12:22:22 +1100

    Set the min "disk" space check in the buildkite cfn-env file

--- packer/oculo-scripts/add-ramdisk-tmpfs.sh
+++ packer/oculo-scripts/add-ramdisk-tmpfs.sh
@@ -2,6 +2,7 @@
 
 set -euxo pipefail
 
-echo 'none /var/lib/docker tmpfs size=12G' | sudo tee -a /etc/fstab >/dev/null
+sudo mkdir -p /var/lib/buildkite-agent/builds
+sudo chown buildkite-agent:buildkite-agent /var/lib/buildkite-agent/builds
 echo 'none /var/lib/buildkite-agent/builds tmpfs size=12G' | sudo tee -a /etc/fstab >/dev/null
-echo 'export DISK_MIN_AVAILABLE=976563' | sudo tee -a /var/lib/buildkite-agent/.bashrc >/dev/null
+echo 'none /var/lib/docker tmpfs size=12G' | sudo tee -a /etc/fstab >/dev/null
