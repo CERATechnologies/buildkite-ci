91ab31d1fabf16a6a55d3f9b1b02c74839a353a3
Joseph Miller <joff@joff.codes>
2020-01-07 14:50:42 +1100

    Re-balance the RAM disk size allocation

--- packer/oculo-scripts/add-ramdisk-tmpfs.sh
+++ packer/oculo-scripts/add-ramdisk-tmpfs.sh
@@ -4,5 +4,5 @@ set -euxo pipefail
 
 sudo mkdir -p /var/lib/buildkite-agent/builds
 sudo chown buildkite-agent:buildkite-agent /var/lib/buildkite-agent/builds
-echo 'none /var/lib/buildkite-agent/builds tmpfs size=12G' | sudo tee -a /etc/fstab >/dev/null
-echo 'none /var/lib/docker tmpfs size=12G' | sudo tee -a /etc/fstab >/dev/null
+echo 'none /var/lib/buildkite-agent/builds tmpfs size=3G' | sudo tee -a /etc/fstab >/dev/null
+echo 'none /var/lib/docker tmpfs size=21G' | sudo tee -a /etc/fstab >/dev/null
